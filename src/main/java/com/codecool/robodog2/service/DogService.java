d98e4fd58618a0ff19ca2e592a918f929c2b10f3
hojo84 <hojo84@gmail.com>
2022-04-26 16:07:35 +0200

    Add class DogService to serve out requests and DAO operations

---
+++ src/main/java/com/codecool/robodog2/service/DogService.java
@@ -0,0 +1,43 @@
+package com.codecool.robodog2.service;
+
+import com.codecool.robodog2.dao.DogDAO;
+import com.codecool.robodog2.model.Dog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DogService {
+
+    private final DogDAO dogDAO;
+
+    @Autowired
+    public DogService(DogDAO dogDAO) {
+        this.dogDAO = dogDAO;
+    }
+
+    public void addDog(Dog dog) {
+        dogDAO.addDog(dog);
+    }
+
+    public List<Dog> listDogs() {
+        return dogDAO.listDogs();
+    }
+
+    public Dog getDog(long id) {
+        return dogDAO.getDog(id);
+    }
+
+    public void updateDog(Dog dog, long id) {
+        dogDAO.updateDog(dog, id);
+    }
+
+    public void deleteDog(long id) {
+        dogDAO.deleteDog(id);
+    }
+
+    public long addDogAndReturnId(Dog dog) {
+        return dogDAO.addDogAndReturnId(dog);
+    }
+}
