2a04785f798d1326f23d199df615308c317484c8
hojo84 <hojo84@gmail.com>
2022-04-27 16:48:10 +0200

    Implement TrickDAO interface methods in TrickJdbcDao

--- src/main/java/com/codecool/robodog2/dao/TrickJdbcDao.java
+++ src/main/java/com/codecool/robodog2/dao/TrickJdbcDao.java
@@ -2,6 +2,9 @@ package com.codecool.robodog2.dao;
 
 import com.codecool.robodog2.dao.mapper.TrickMapper;
 import com.codecool.robodog2.model.Trick;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +13,8 @@ import java.util.Optional;
 
 @Repository
 public class TrickJdbcDao implements TrickDAO {
+
+    private static final Logger log = LoggerFactory.getLogger(TrickJdbcDao.class);
 
     private final JdbcTemplate jdbcTemplate;
     private final TrickMapper trickMapper;
@@ -21,26 +26,37 @@ public class TrickJdbcDao implements TrickDAO {
 
     @Override
     public void addTrick(Trick trick) {
-
+        String sql = "INSERT INTO trick(name) VALUES (?)";
+        jdbcTemplate.update(sql, trick.getName());
     }
 
     @Override
     public List<Trick> listTricks() {
-        return null;
+        String sql = "SELECT id, name FROM trick";
+        return jdbcTemplate.query(sql, trickMapper);
     }
 
     @Override
     public Optional<Trick> getTrick(long id) {
-        return Optional.empty();
+        String sql = "SELECT id, name FROM trick WHERE id=?";
+        Trick trick = null;
+        try {
+            trick = jdbcTemplate.queryForObject(sql, trickMapper, id);
+        } catch (DataAccessException e) {
+            log.info("Trick not found: " + id);
+        }
+        return Optional.ofNullable(trick);
     }
 
     @Override
     public void updateTrick(Trick trick, long id) {
-
+        String sql = "UPDATE trick SET name=? WHERE id=?";
+        jdbcTemplate.update(sql, trick.getName(), id);
     }
 
     @Override
     public void deleteTrick(long id) {
-
+        String sql = "DELETE FROM trick WHERE id=?";
+        jdbcTemplate.update(sql, id);
     }
 }
