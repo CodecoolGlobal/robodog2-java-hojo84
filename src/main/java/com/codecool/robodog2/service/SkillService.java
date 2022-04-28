77235a3e5631244042ae171f2ded571002be01b2
hojo84 <hojo84@gmail.com>
2022-04-28 16:18:22 +0200

    Create class SkillService

---
+++ src/main/java/com/codecool/robodog2/service/SkillService.java
@@ -0,0 +1,48 @@
+package com.codecool.robodog2.service;
+
+import com.codecool.robodog2.dao.SkillDAO;
+import com.codecool.robodog2.model.Dog;
+import com.codecool.robodog2.model.Skill;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SkillService {
+
+    private final SkillDAO skillDAO;
+
+    @Autowired
+    public SkillService(SkillDAO skillDAO) {
+        this.skillDAO = skillDAO;
+    }
+
+    public void addSkill(Skill skill) {
+        skillDAO.addSkill(skill);
+    }
+
+    public List<Skill> listSkills() {
+        return skillDAO.listSkills();
+    }
+
+    public Skill getSkill(long id) {
+        return skillDAO.getSkill(id).orElseThrow();
+    }
+
+    public void updateSkill(Skill skill, long id) {
+        skillDAO.updateSkill(skill, id);
+    }
+
+    public void deleteSkill(long id) {
+        skillDAO.deleteSkill(id);
+    }
+
+    public List<Dog> listDogsByTrickId(long trickId) {
+        return skillDAO.listDogsByTrickId(trickId);
+    }
+
+    public Skill getSkillByDogIdAndTrickId(long dogId, long trickId) {
+        return skillDAO.getSkillByDogIdAndTrickId(dogId, trickId).orElseThrow();
+    }
+}
