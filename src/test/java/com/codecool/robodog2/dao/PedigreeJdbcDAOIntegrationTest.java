47f74d2903530d951f7c3dbaab349a2f65cfbf43
hojo84 <hojo84@gmail.com>
2022-05-01 14:54:56 +0200

    Create tests for PedigreeJdbcDao

--- src/test/java/com/codecool/robodog2/dao/PedigreeJdbcDAOIntegrationTest.java
+++ src/test/java/com/codecool/robodog2/dao/PedigreeJdbcDAOIntegrationTest.java
@@ -1,18 +1,72 @@
 package com.codecool.robodog2.dao;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import com.codecool.robodog2.model.Breed;
+import com.codecool.robodog2.model.Dog;
+import com.codecool.robodog2.model.Pedigree;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@JdbcTest
 //Use the schema built in the .sql file put in resources
-@Sql(scripts = {"classpath:schema.sql"})
+@Sql(scripts = {"classpath:schema.sql", "classpath:data.sql"})
 @ComponentScan
-@SpringBootTest
+//@SpringBootTest
 //As the context becomes dirty - database modifications happen, etc. - this method restores it to the original state
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class PedigreeJdbcDAOIntegrationTest {
 
-    //TODO
+    @Autowired
+    private PedigreeDAO pedigreeDAO;
+    @Qualifier("dogJdbcDao")
+    @Autowired
+    DogDAO dogDAO;
 
+    @Test
+    public void testGetPedigreeByDogId() {
+        Pedigree expected = new Pedigree();
+        expected.setPuppyId(3);
+        expected.setMomId(1);
+        expected.setDadId(2);
+        final Pedigree actual = pedigreeDAO.getPedigreeByPuppyId(3).orElseThrow();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetMomByDogId() {
+        Dog expectedMom = new Dog(Breed.SPANIEL, "Lucy", 2);
+        final Pedigree pedigreeByPuppyId = pedigreeDAO.getPedigreeByPuppyId(3).orElseThrow();
+        final long momId = pedigreeByPuppyId.getMomId();
+        final Dog actualMom = dogDAO.getDog(momId).orElseThrow();
+        assertEquals(expectedMom, actualMom);
+    }
+
+    @Test
+    public void testGetDadByDogId() {
+        Dog expectedDad = new Dog(Breed.LABRADOR, "Molly", 1);
+        final Pedigree pedigreeByPuppyId = pedigreeDAO.getPedigreeByPuppyId(3).orElseThrow();
+        final long dadId = pedigreeByPuppyId.getDadId();
+        final Dog actualDad = dogDAO.getDog(dadId).orElseThrow();
+        assertEquals(expectedDad, actualDad);
+    }
+
+    @Test
+    public void getAllSiblingsByDogId() {
+        Pedigree newPedigree = new Pedigree();
+        newPedigree.setPuppyId(4);
+        newPedigree.setMomId(5);
+        newPedigree.setDadId(2);
+        pedigreeDAO.addPedigree(newPedigree);
+        final List<Long> expectedSiblingIds = List.of(3L, 4L);
+        final List<Long> actualSiblingIds = pedigreeDAO.getSiblingsByPuppyId(3);
+        assertEquals(expectedSiblingIds, actualSiblingIds);
+    }
 }
